e909524ad6ecc7bd912430f20c05c70280d8cb73
lzhtmxsf <741614313@qq.com>
2015-12-17 22:33:24 +0800

    add

---
+++ Global.java
@@ -0,0 +1,12 @@
+package GA;
+public class Global { 
+
+	 public final static int POPU_NUM = 4; //种群的规模 
+
+	 public final static int MAX_GEN_NUM = 8; //遗传的最大代数 
+
+	 public final static double CROSS_RATE = 1; //交叉率 
+
+	 public final static double MUTA_RATE = 0.1; //变异率 
+
+	} 
